d4777ffeaf396eec76b49533a7dda09985e1a0cb
Prem-kumar27 <sprem1997@gmail.com>
2020-03-20 19:33:22 +0530

    Add Payload JSON made by Avik

--- main.py
+++ main.py
@@ -4,42 +4,82 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return 'Hello World!'
+    return 'Hello World!'
 
-def find_intent_entity_match(intent,entities):
-    data=pd.read_json("CoViD_19Mar20.json")
+
+def find_intent_entity_match(intent, entities):
+    data = pd.read_json("CoViD_19Mar20.json")
     data = data[data["Intent"] == intent]
-    data["intent_match_count"] = data["Entities"].apply(lambda x: len(set(x) & set(entities)))
-    answer=data.loc[data['intent_match_count'].idxmax()]
+    data["intent_match_count"] = data["Entities"].apply(
+        lambda x: len(set(x) & set(entities)))
+    answer = data.loc[data['intent_match_count'].idxmax()]
     return answer
 
+
 def results():
-  req = request.get_json(force=True)
-  params = req["queryResult"]["parameters"]
-  entities = {k: v for k, v in params.items() if v is not ""}
-  intent = req["queryResult"]["intent"]["displayName"]
-  
-  result=find_intent_entity_match(intent,entities.keys())
-  textAnswer=result["Answer"]
-  imageURL = result["Image_URL"]
-  response = textAnswer+" Entities: "+",".join(entities)+" Intent: "+intent
-  return {'fulfillmentText': response, 'image':imageURL}
+    req = request.get_json(force=True)
+    params = req["queryResult"]["parameters"]
+    entities = {k: v for k, v in params.items() if v is not ""}
+    intent = req["queryResult"]["intent"]["displayName"]
+
+    result = find_intent_entity_match(intent, entities.keys())
+    textAnswer = result["Answer"] if result["Answer"] else ""
+    textAnswer = textAnswer.replace("\r\n", "\n")
+    print("textAnswer", textAnswer)
+    imageURL = result["Image_URL"]
+    referenceURL = result["Reference"]
+    response = textAnswer+" Entities: " + \
+        ",".join(entities.keys())+" Intent: "+intent
+    reply = {}
+    reply["payload"] = {
+        "google": {
+            "expectUserResponse": True,
+            "richResponse": {
+                "items": [
+                  {
+                      "basicCard": {
+                          "title": "",
+                          "formattedText": response,
+                          "image": {
+                              "url": imageURL,
+                          },
+                          "buttons": [
+                              {
+                                  "title": "See More",
+                                  "openUrlAction": {
+                                      "url": referenceURL
+                                  }
+                              }
+                          ],
+                          "imageDisplayOptions": "CROPPED"
+                      },
+                      "suggestions": [
+                          {
+                          }
+                      ],
+                  }
+                ],
+            }
+        }
+    }
+    return reply
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-  return make_response(jsonify(results()))
+    return make_response(jsonify(results()))
 
 # Example: https://webhook-dot-covid-bot.appspot.com/test/what-is/COVID-19,OK
 @app.route('/test/<path:query>', methods=['GET', 'POST'])
 def test(query):
-  intent, entities = query.split('/')
-  entities = entities.split(',')
-  response = find_intent_entity_match(intent, entities).to_json(indent=4)
-  return make_response(response)
+    intent, entities = query.split('/')
+    entities = entities.split(',')
+    response = find_intent_entity_match(intent, entities).to_json(indent=4)
+    return make_response(response)
+
 
 if __name__ == '__main__':
-  app.run(port=8000,debug=True)
-
+    app.run(port=8000, debug=True)
