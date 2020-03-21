3053fa08b47c04c99ba2e82ea27e79f21fef554b
Pratyush Kumar <pratyushkpanda@gmail.com>
2020-03-21 22:10:29 +0530

    Small typos in the initial list of what-is questions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 import pandas as pd
 import random
+import datetime
 import json
 
 app = Flask(__name__)
@@ -22,21 +23,53 @@ CORONA_JSON = 'coronabot_qa_data.json'
 corona_data = pd.read_json(CORONA_JSON)
 corona_data['EntitiesStr'] = corona_data['Entities'].apply(entity_stringify)
 
+STATS_CSV = 'coronabot_stats_data.csv'
+stats_data = pd.read_csv(STATS_CSV)
+stats_data['Date'] = stats_data['Date'].apply(lambda x: x[0:10])
+
+failure_messages = ["Sorry I could not understand you", "I am sorry, I did not follow", "Can you please rephrase that"]
+
 @app.route('/') 
 def index():
   return 'Hello World!'
 
-# def find_intent_entity_match(intent, entities):
-#     data = corona_data[corona_data["Intent"] == intent]
-#     print(intent, entities);
-#     data["intent_match_count"] = data["Entities"].apply(
-#         lambda x: len(set(x) & set(entities)))
-#     answer = data.loc[data['intent_match_count'].idxmax()]
-#     return answer
+def get_stats(intent, entities):
 
-failure_messages = ["Sorry I could not understand you", "I am sorry, I did not follow", "Can you please rephrase that"]
+  country = entities['geo-country'] if len(entities['geo-country']) > 0 else 'World'
+  state = entities['geo-state'] if len(entities['geo-state']) > 0 else 'Total'
+  case_type = entities['case_types'] if len(entities['case_types']) > 0 else 'Confirmed'
+  yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
+  date = entities['date-time'][0:10] if len(entities['date-time']) > 0 else yesterday.strftime("%Y-%m-%d")
+
+
+  if not state == 'Total':
+    stats_data_sel = stats_data[(stats_data['State'] == state) & (stats_data['Date'] == date)]
+    str_location = state + ', ' + stats_data_sel['Country']
+  else:
+    stats_data_sel = stats_data[(stats_data['Country'] == country) & (stats_data['State'] == state) & (stats_data['Date'] == date)]
+    str_location = country
+
+  if case_type == 'deaths':
+    case_type = 'Deaths'
+    str_case_type = 'deaths'
+  elif case_type == 'recovered':
+    case_type = 'Recovered'
+    str_case_type = 'patients who were reported to be recovered'
+
+  ret_val = stats_data_sel.iloc[0][case_type]
+  response = {
+      "Response_Type": "Error:EntitiesNotFound",
+      "Answer": 'In ' + str_location + ' there were ' + str(ret_val) + ' ' + str_case_type + ' on ' + date
+  }
+  return response
 
 def find_answer(intent, entities):
+
+
+  if intent[0:5] == 'stats':
+    return get_stats(intent, entities)
+
+  entities = entity_stringify(entities)
 
   data_match_intent = corona_data[corona_data["Intent"] == intent]
 
@@ -63,13 +96,13 @@ def find_answer(intent, entities):
 def results():
 
   req = request.get_json(force=True)
+
   # find intent
   intent = req["queryResult"]["intent"]["displayName"]
   # find entities
   params = req["queryResult"]["parameters"]
 
-  entities = entity_stringify(params)
-  result = find_answer(intent, entities)
+  result = find_answer(intent, params)
 
   if "Error" in result["Response_Type"]:
     reply = {}
@@ -89,48 +122,56 @@ def results():
     response = textAnswer # + " Entities: " + ",".join(entities)+" Intent: " + intent
 
     reply = {}
-    reply["payload"] = {
-      "google": 
-      {
-        "expectUserResponse": True,
-        "richResponse": 
+    reply = {
+      "fulfillmentText": "",
+      "fulfillmentMessages": [
         {
-          "items": [
+        }
+      ],
+      "source": "example.com",
+      "payload": {
+        "google": 
+        {
+          "expectUserResponse": True,
+          "richResponse": 
           {
-            "basicCard": 
+            "items": [
             {
-              "title": textTitle,
-              "formattedText": response,
-              "image": 
+              "basicCard": 
               {
-                "url": imageURL,
+                "title": textTitle,
+                "formattedText": response,
+                "image": 
+                {
+                  "url": imageURL,
+                },
+                "buttons": [
+                {
+                  "title": "Source",
+                  "openUrlAction": 
+                  {
+                      "url": sourceURL
+                  }
+                }, 
+                {
+                  "title": "Learn more",
+                  "openUrlAction": 
+                  {
+                    "url": referenceURL
+                  }
+                }
+                ],
+                "imageDisplayOptions": "CROPPED"
               },
-              "buttons": [
-              {
-                "title": "Source",
-                "openUrlAction": 
-                {
-                    "url": sourceURL
-                }
-              }, 
-              {
-                "title": "Learn more",
-                "openUrlAction": 
-                {
-                  "url": referenceURL
-                }
-              }
-              ],
-              "imageDisplayOptions": "CROPPED"
-            },
+            }
+            ],
+            "suggestions": suggestions
           }
-          ],
-          "suggestions": suggestions
         }
       }
     }
 
-    return reply
+  return reply
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
